742729e2a4c5c259f9c06c02eb663c9f9d018d81
trifo13 <trifo13@gmail.com>
2024-03-21 08:52:38 +0200

    Updated output in 02-02.

--- 02-02-operator-workspace/outputs.tf
+++ 02-02-operator-workspace/outputs.tf
@@ -1,3 +1,8 @@
+output "secret_private_ec2_ip" {
+  description = "The Private IPv4 address of the EC2 target provision by the Operator:"
+  value       = aws_instance.main.private_ip
+}
+
 output "aws_creds_op_key" {
   description = "Operator key verification"
   value       = data.vault_aws_access_credentials.creds.access_key
