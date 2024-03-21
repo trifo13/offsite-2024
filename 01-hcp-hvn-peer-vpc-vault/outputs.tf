f3fbd96122c5df6ccc7fedde624a26ca9958ce41
trifo13 <trifo13@gmail.com>
2024-03-21 21:07:58 +0200

    Updated the 01 outputs.

--- 01-hcp-hvn-peer-vpc-vault/outputs.tf
+++ 01-hcp-hvn-peer-vpc-vault/outputs.tf
@@ -1,4 +1,9 @@
+output "AWS_VPC_ID" {
+  description = "AWS VPC ID: to be used as variable in TFC Workspace:"
+  value       = aws_vpc.peer.id
+}
+
 output "VAULT_ADDR" {
-  description = "HCP Vault public URL to be used as variable in TFC Workspace:"
+  description = "HCP Vault public URL: to be used as variable in TFC Workspace:"
   value       = hcp_vault_cluster.offsite-2024.vault_public_endpoint_url
 }
