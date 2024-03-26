e0d31e8f77121b843e450ab420556ae77fede7fa
trifo13 <trifo13@gmail.com>
2024-03-26 15:07:46 +0200

    Updates to 03-01 - TT.

--- 03-01-hcp-boundary/03-01-hcp-boundary.tf
+++ 03-01-hcp-boundary/03-01-hcp-boundary.tf
@@ -49,28 +49,22 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
-resource "aws_subnet" "hvn_vpc" {
-  vpc_id     = hcp_aws_network_peering.peer.peer_vpc_id
-  cidr_block = "172.31.0.0/20"
 
-  availability_zone = "us-west-2c"
-}
 
 # RSA key of size 4096 bits
-resource "tls_private_key" "rsa_4096_key" {
+resource "tls_private_key" "private-ec2_rsa_4096_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "ec2_key" {
-  key_name   = "ec2-key"
-  public_key = trim("${tls_private_key.rsa_4096_key.public_key_openssh}", "\n")
-
+resource "aws_key_pair" "private-ec2_key" {
+  key_name   = "private-ec2-key"
+  public_key = trim("${tls_private_key.private-ec2_rsa_4096_key.public_key_openssh}", "\n")
 
   provisioner "local-exec" {
     command = <<-EOT
-    echo '${tls_private_key.rsa_4096_key.private_key_pem}' > ec2-key.pem
-      chmod 400 ec2-key.pem
+    echo '${tls_private_key.private-ec2_rsa_4096_key.private_key_pem}' > private-ec2-key.pem
+      chmod 400 private-ec2-key.pem
     EOT
   }
 }
@@ -78,12 +72,12 @@ resource "aws_key_pair" "ec2_key" {
 resource "aws_instance" "main" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.hvn_vpc.id
-  key_name      = aws_key_pair.ec2_key.key_name
+  subnet_id     = aws_subnet.public_subnets[0].id
+  key_name      = aws_key_pair.private-ec2_key.key_name
   tags = {
     Name  = "Super-secret-EC2-instance"
     TTL   = var.ttl
-    Owner = "${var.project_name}-guide"
+    Owner = "${var.project_name}-operator"
   }
 }
 
@@ -106,50 +100,6 @@ resource "aws_iam_user_policy" "secrets_engine" {
     ]
     Version = "2012-10-17"
   })
-}
-
-resource "aws_vpc" "peer" {
-  cidr_block = "172.31.0.0/16"
-}
-
-resource "aws_vpc_peering_connection_accepter" "peer" {
-  vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
-  auto_accept               = true
-}
-
-data "aws_arn" "peer" {
-  arn = aws_vpc.peer.arn
-}
-
-resource "hcp_aws_network_peering" "peer" {
-  hvn_id          = hcp_hvn.offsite-2024.hvn_id
-  peering_id      = var.peering_id
-  peer_vpc_id     = aws_vpc.peer.id
-  peer_account_id = aws_vpc.peer.owner_id
-  peer_vpc_region = data.aws_arn.peer.region
-}
-
-# Create HCP HVN:
-resource "hcp_hvn" "offsite-2024" {
-  hvn_id         = var.hvn_id
-  cloud_provider = var.cloud_provider
-  region         = var.region
-}
-
-resource "hcp_hvn_route" "peer_route" {
-  hvn_link         = hcp_hvn.offsite-2024.self_link
-  hvn_route_id     = var.route_id
-  destination_cidr = aws_vpc.peer.cidr_block
-  target_link      = hcp_aws_network_peering.peer.self_link
-}
-
-
-# Create HCP Vault cluster:
-resource "hcp_vault_cluster" "offsite-2024" {
-  hvn_id          = hcp_hvn.offsite-2024.hvn_id
-  cluster_id      = var.cluster_id
-  tier            = var.hcp-vault_tier
-  public_endpoint = true
 }
 
 # Ask TFC to create a role for the AWS Secret Engine with a policy that allows the role access to iam and ec2:
@@ -177,4 +127,74 @@ EOF
 # Dynamically generate token a for HCP Vault using the variables we set in TFC:
 resource "hcp_vault_cluster_admin_token" "hcp-v-offsite-2024" {
   cluster_id = var.cluster_id
+}
+
+resource "hcp_vault_cluster" "offsite-2024" {
+  hvn_id          = hcp_hvn.offsite-2024.hvn_id
+  cluster_id      = var.cluster_id
+  tier            = var.hcp-vault_tier
+  public_endpoint = true
+  depends_on      = [aws_internet_gateway.hvn-gw]
+}
+
+# Create HCP HVN:
+resource "hcp_hvn" "offsite-2024" {
+  hvn_id         = var.hvn_id
+  cloud_provider = var.cloud_provider
+  region         = var.region
+}
+
+resource "aws_vpc" "peer" {
+  cidr_block = "172.31.0.0/16"
+}
+
+# Create HCP HVN Peering to AWS VPC:
+data "aws_arn" "peer" {
+  arn = aws_vpc.peer.arn
+}
+
+resource "hcp_aws_network_peering" "peer" {
+  hvn_id          = hcp_hvn.offsite-2024.hvn_id
+  peering_id      = var.peering_id
+  peer_vpc_id     = aws_vpc.peer.id
+  peer_account_id = aws_vpc.peer.owner_id
+  peer_vpc_region = data.aws_arn.peer.region
+}
+
+resource "hcp_hvn_route" "peer_route" {
+  hvn_link         = hcp_hvn.offsite-2024.self_link
+  hvn_route_id     = var.route_id
+  destination_cidr = aws_vpc.peer.cidr_block
+  target_link      = hcp_aws_network_peering.peer.self_link
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
+  auto_accept               = true
+}
+
+# Create AWS Internet gateway:
+resource "aws_internet_gateway" "hvn-gw" {
+  vpc_id = aws_vpc.peer.id
+}
+
+resource "aws_route_table" "hvn_second_rt" {
+  vpc_id = aws_vpc.peer.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.hvn-gw.id
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count      = length(var.subnet_cidrs)
+  vpc_id     = aws_vpc.peer.id
+  cidr_block = element(var.subnet_cidrs, count.index)
+}
+
+resource "aws_route_table_association" "subnet_asso" {
+  count          = length(var.subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.hvn_second_rt.id
 }
